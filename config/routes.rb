d38c45f1aef3406be24d07f29d3326e4f51d9021
jessi.iler <inertial.observer@gmail.com>
2015-07-20 20:21:55 -0700

    add routes, skill index works

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/about'
+  root :to => 'skills#index'
 
-  get 'pages/contact'
+resources :skills do
+resources :projects, :except =>[:index]
+end
+  # get 'pages/home'
+  #
+  # get 'pages/about'
+  #
+  # get 'pages/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
